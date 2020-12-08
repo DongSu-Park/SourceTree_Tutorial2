751e8701e2b1719fe9361558e403e1bc320a3a93
Park_Dong_Su <floreerin@gmail.com>
2020-12-08 13:11:48 +0900

    MainActivity UI Update, Add Button Click for Toast Message

--- app/src/main/java/com/example/sourcetree_tutorial2/MainActivity.kt
+++ app/src/main/java/com/example/sourcetree_tutorial2/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.sourcetree_tutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        btn_toast.setOnClickListener {
+            Toast.makeText(this, "테스트 메세지 입니다.", Toast.LENGTH_LONG).show()
+        }
+    }
 }
